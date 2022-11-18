cb91b8f280909611991387798830f0d61c71980b
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-18 16:17:48 +0500

    first_item

--- first_item.py
+++ first_item.py
@@ -3,4 +3,8 @@ def first_item():
     Given a dictionary, Return first item value.
     """
     data = {'a': 1, 'b': 2}
-    return
+    while data:
+        k,v=data.popitem()
+    return v
+print(first_item())
+
